7f13634e1ca39e61d7740706e51b2ae36542b683
Pacchiotti Marco <57013700+pacchio1@users.noreply.github.com>
2024-02-04 22:48:31 +0100

    fix +tmux launcher

--- +bashrc.sh
+++ +bashrc.sh
@@ -19,7 +19,16 @@ alias i3status-config='nvim ~/.config/i3status/config'
 
 #programmi
 # Dracula theme for Bash
-export PS1="\[\033[38;5;204m\]\u\[$(tput sgr0)\]@\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;>
-neofetch
+export PS1="\[\033[38;5;204m\]\u\[$(tput sgr0)\]@\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;108m\]\h\[$(tput sgr0)\]:\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;216m\]\w\[$(tput sgr0)\]\[\033[38;5;37m\]\$ \[$(tput sgr0)\]"
 
+neofetch
+#tmux
+# Verifica se la variabile di controllo è impostata e se non siamo già dentro una sessione tmux
+if [ -z "$TMUX_STARTED" ] && [ -z "$TMUX" ]; then
+            # Esegui il comando solo se la variabile non è impostata e non siamo già dentro una sessione tmux
+                tmux
+
+                    # Imposta la variabile di controllo
+                        export TMUX_STARTED=1
+fi
 #path variabili
