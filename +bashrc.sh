b375c8125c4d744084791cd98ec4399137677a84
Pacchiotti Marco <57013700+pacchio1@users.noreply.github.com>
2024-02-08 15:46:45 +0100

    Update +bashrc.sh

--- +bashrc.sh
+++ +bashrc.sh
@@ -3,19 +3,7 @@
 # aggiungere
 HISTSIZE=25000
 HISTFILESIZE=25000
-#alias
-alias ll='ls -la --color=auto'
-alias ls='exa'
-alias gc='git commit -m'
-alias gf='git fetch'
-alias gck='git checkout'
-alias gpl='git pull'
-alias gph='git push'
-alias vim='nvim'
-alias ip='ip -c a'
-alias aur-install='makepkg -si'
-alias i3config='nvim ~/.config/i3/config'
-alias i3status-config='nvim ~/.config/i3status/config'
+
 
 #programmi
 # Dracula theme for Bash
@@ -32,3 +20,28 @@ if [ -z "$TMUX_STARTED" ] && [ -z "$TMUX" ]; then
                         export TMUX_STARTED=1
 fi
 #path variabili
+
+#Alias 
+
+#commands
+alias ll='ls -la --color=auto'
+alias ls='exa'
+alias gc='git commit -m'
+alias gf='git fetch'
+alias gck='git checkout'
+alias gpl='git pull'
+alias gph='git push'
+alias vim='nvim'
+alias ip='ip -c a'
+alias aur-install='makepkg -si'
+alias i3config='nvim ~/.config/i3/config'
+alias i3status-config='nvim ~/.config/i3status/config'
+
+#Angular
+alias ngs='ng serve'
+alias nggc='ng generate component'
+alias nggm='ng generate module'
+alias nggs='ng generate service'
+#node
+alias npmi='npm install'
+alias npms='npm start'
