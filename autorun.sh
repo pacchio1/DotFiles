0bbe977c842e73c6daa8efad9e3a0c2f2c31e478
Pacchiotti Marco <57013700+pacchio1@users.noreply.github.com>
2024-02-15 00:22:57 +0100

    wallPAPER!!!!

--- autorun.sh
+++ autorun.sh
@@ -6,11 +6,14 @@ links(){
     ln -sf ~/git/DotFiles/.config/terminator/config ~/.config/terminator/config
     ln -sf ~/git/DotFiles/.tmux.conf ~/.tmux.conf
     ln -sf ~/git/DotFiles/.gitconfig ~/.gitconfig
+    mkdir wallpaper
+    ln -sf ~/git/DotFiles/wallpaper/* ~/wallpaper
     mkdir .local/share/fonts
     cp -r ~/git/DotFiles/.local/share/fonts/ ~/.local/share/fonts
     mv ~/.config/nvim{,.bak}
     git clone https://github.com/LazyVim/starter ~/.config/nvim
     rm -rf ~/.config/nvim/.git
+    cd ~/wallpaper && mkdir wallpaper-drive && cd wallpaper-drive && wget https://drive.google.com/file/d/1GC5mJHJsalAbE9mIRFUqP-9knfNU1Ykr/view?usp=drive_link
 }
 
 ask_confirmation() {
