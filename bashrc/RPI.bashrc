f0e25e60d5e4aa5431ee8b6e720d0b458d8209e7
Marco Pacchiotti <marco.pacchiotti@excentio.com>
2024-02-14 12:54:36 +0100

    rename

---
+++ bashrc/RPI.bashrc
@@ -0,0 +1,8 @@
+# Del Raspberrypi
+
+#Personalizazione
+#free -h
+neofetch --ascii_distro raspbian --ascii_color 2 1 2 2 1 1
+df -h
+echo ""
+echo "remember to use nala -> apt"
---
+++ bashrc/daily.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+# Confisurazione BASH
+# If not running interactively, don't do anything
+case $- in
+*i*) ;;
+*) return ;;
+esac
+
+HISTSIZE=25000
+HISTFILESIZE=25000
+
+shopt -s histappend
+shopt -s checkwinsize
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+	. ~/.bash_aliases
+fi
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+	if [ -f /usr/share/bash-completion/bash_completion ]; then
+		. /usr/share/bash-completion/bash_completion
+	elif [ -f /etc/bash_completion ]; then
+		. /etc/bash_completion
+	fi
+fi
+
+#programmi
+# Dracula theme for Bash
+export PS1="\[\033[38;5;204m\]\u\[$(tput sgr0)\]@\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;108m\]\h\[$(tput sgr0)\]:\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;216m\]\w\[$(tput sgr0)\]\[\033[38;5;37m\]\$ \[$(tput sgr0)\]"
+
+neofetch
+#tmux
+# Verifica se la variabile di controllo è impostata e se non siamo già dentro una sessione tmux
+if [ -z "$TMUX_STARTED" ] && [ -z "$TMUX" ]; then
+	# Esegui il comando solo se la variabile non è impostata e non siamo già dentro una sessione tmux
+	tmux
+
+	# Imposta la variabile di controllo
+	export TMUX_STARTED=1
+fi
+#path variabili
+
+#Alias
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+	test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+	alias ls='ls --color=auto'
+	#alias dir='dir --color=auto'
+	#alias vdir='vdir --color=auto'
+
+	alias grep='grep --color=auto'
+	alias fgrep='fgrep --color=auto'
+	alias egrep='egrep --color=auto'
+fi
+#vpn
+alias vpn-excentio="cd ~ && sudo openvpn marcopacchiotti.ovpn"
+#commands
+alias ll='ls -la --color=auto'
+alias ls='exa'
+alias gc='git commit -m'
+alias gf='git fetch'
+alias gck='git checkout'
+alias gpl='git pull'
+alias gph='git push'
+alias vim='nvim'
+alias ip='ip -c a'
+alias aur-install='makepkg -si'
+alias i3config='nvim ~/.config/i3/config'
+alias i3status-config='nvim ~/.config/i3status/config'
+alias cd2='cd ../..'
+alias cd3='cd ../../..'
+alias gitf='cd && cd git'
+alias dcu='docker compose up -d'
+alias dcd='docker compose down'
+alias podf='cd && cd docker'
+
+#Angular
+# Load Angular CLI autocompletion.
+source <(ng completion script)
+alias ngs='ng serve'
+alias nggc='ng generate component'
+alias nggm='ng generate module'
+alias nggs='ng generate service'
+#node
+alias npmi='npm install'
+alias npms='npm start'
+#Test git
+alias dff='gitf && cd DotFiles'
