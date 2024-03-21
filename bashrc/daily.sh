549704ef7c8e3359c3133798188112074627a9dd
Marco Pacchiotti <57013700+pacchio1@users.noreply.github.com>
2024-03-21 17:27:48 +0100

    ciao

--- bashrc/daily.sh
+++ bashrc/daily.sh
@@ -88,3 +88,7 @@ if [ -x /usr/bin/dircolors ]; then
 	alias egrep='egrep --color=auto'
 fi
 
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
