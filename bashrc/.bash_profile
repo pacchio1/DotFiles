2dbe134024b4d38e460ce0231d382e0c1e322fb5
Marco Pacchiotti <marco.pacchiotti@excentio.com>
2024-06-01 17:08:18 +0200

    fix bash

--- bashrc/.bash_profile
+++ bashrc/.bash_profile
@@ -9,6 +9,29 @@ fi
 
 . "$HOME/.cargo/env"
 
-#Export
+#doom emacs
+export PATH=$PATH:~/.config/emacs/bin
+#rust
+if [ -f "$HOME/.cargo/env" ]; then
+	. "$HOME/.cargo/env"
+fi
+export GTK_THEME=Dracula
+export ICONS_THEME=Papirus
 
+export PYENV_ROOT="$HOME/.pyenv"
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+#export PATH=$PATH:/usr/share/sdk-android-tool/platform-tools
+
+if [ -f "$HOME/.pyenv" ]; then
+
+	export PYENV_ROOT="$HOME/.pyenv"
+	export PYENV_ROOT="$HOME/.pyenv"
+	[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+	eval "$(pyenv init -)"
+fi
+#Export
+export SDKMAN_DIR="$HOME/.sdkman"
 export portatile=192.168.1.22
--- bashrc/daily.sh
+++ bashrc/daily.sh
@@ -34,32 +34,8 @@ fi
 
 ########  path variabili ##
 
-#doom emacs
-export PATH=$PATH:~/.config/emacs/bin
-#rust
-if [ -f "$HOME/.cargo/env" ]; then
-	. "$HOME/.cargo/env"
-fi
-export GTK_THEME=Dracula
-export ICONS_THEME=Papirus
+# .bash_profile
 
-<<<<<<< HEAD
-export PYENV_ROOT="$HOME/.pyenv"
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-
-#export PATH=$PATH:/usr/share/sdk-android-tool/platform-tools
-
-=======
-if [ -f "$HOME/.pyenv" ]; then
-
-	export PYENV_ROOT="$HOME/.pyenv"
-	export PYENV_ROOT="$HOME/.pyenv"
-	[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-	eval "$(pyenv init -)"
-fi
->>>>>>> a4e07c15f8e9165e665e2bea0262f47c634a68ba
 ########## tmux ##########
 
 # Verifica se la variabile di controllo è impostata e se non siamo già dentro una sessione tmux
@@ -116,5 +92,4 @@ if command -v ng &>/dev/null; then
 fi
 
 #THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
-export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
