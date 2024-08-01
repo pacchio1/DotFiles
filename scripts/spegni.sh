1c1bd6e38fcdd65e0846802f08057e8241f7a122
Marco Pacchiotti <marco.pacchiotti@excentio.com>
2024-08-01 14:54:21 +0200

    spegni

---
+++ scripts/spegni.sh
@@ -0,0 +1,15 @@
+ask_confirmation_nvim() {
+	read -p "$1 (y/N): " response
+	case "$response" in
+	[yY])
+		shutdown -h now
+		return 0 # Conferma
+		;;
+	*)
+		return 1 # Non conferma
+		;;
+	esac
+}
+if ask_confirmation "Confermi di spegnere il computer?"; then
+	echo "Fatto! / Done!"
+else
