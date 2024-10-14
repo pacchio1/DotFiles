bf5355699b1ad41f00fafd30b67626a1a8b02c8d
Marco Pacchiotti <marco.pacchiotti@excentio.com>
2024-10-14 23:22:25 +0200

    shutdown fix,  full path music script

--- scripts/spegni.sh
+++ scripts/spegni.sh
@@ -1,17 +1,17 @@
 ask_confirmation() {
-	read -p "$1 (y/N): " response
-	case "$response" in
-	[yYsS])
-		shutdown -h now
-		return 0 # Conferma
-		;;
-	*)
-		return 1 # Non conferma
-		;;
-	esac
+  read -p "$1 (y/N): " response
+  case "$response" in
+  [yYsS])
+    systemctl poweroff -i
+    return 0 # Conferma
+    ;;
+  *)
+    return 1 # Non conferma
+    ;;
+  esac
 }
 if ask_confirmation "Confermi di spegnere il computer?"; then
-	echo "Fatto! / Done!"
+  echo "Fatto! / Done!"
 else
-    echo "Non hai confermato. / Not Done !"
+  echo "Non hai confermato. / Not Done !"
 fi
